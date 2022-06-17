4680a3b7f678d3bdf60c9cb24a6f0e2011bb8c1b
dejwi <misti.krasula@gmail.com>
2022-06-17 18:34:36 +0200

    fixed bad login

--- src/index.ts
+++ src/index.ts
@@ -35,21 +35,25 @@ const Librus = async (username: string, password: string) => {
   let classesApi: ClassesApi;
 
   const login = async () => {
-    const res = await axios.post(
-      "https://api.librus.pl/OAuth/Token",
-      `username=${username}&password=${password}&librus_long_term_token=1&grant_type=password`,
-      {
-        headers: {
-          Authorization:
-            "Basic Mjg6ODRmZGQzYTg3YjAzZDNlYTZmZmU3NzdiNThiMzMyYjE=",
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-    if (res.status !== 200) return false;
+    try {
+      const res = await axios.post(
+        "https://api.librus.pl/OAuth/Token",
+        `username=${username}&password=${password}&librus_long_term_token=1&grant_type=password`,
+        {
+          headers: {
+            Authorization:
+              "Basic Mjg6ODRmZGQzYTg3YjAzZDNlYTZmZmU3NzdiNThiMzMyYjE=",
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      if (res.status !== 200) return false;
 
-    token = `Bearer ${res.data.access_token}`;
-    return true;
+      token = `Bearer ${res.data.access_token}`;
+      return true;
+    } catch (err) {
+      return false;
+    }
   };
 
   const getApi = async (url: string) => {
